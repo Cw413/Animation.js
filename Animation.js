95a8a1fec1ccad3598bb5ac706cae535c7fdc037
rinyPeng <3253421891@qq.com>
2019-11-27 10:18:02 +0800

    Update Animation.js

--- Animation.js
+++ Animation.js
@@ -1,79 +1,101 @@
-const Animation=(options)=>{
+/**
+* 动画
+* @author pjw
+* ------------
+* @options
+*	- elm 动画元素
+*	- time 动画完成消耗时间
+*	- actions 动画集合
+*	- callback 动画完成回调
+*/
 
-	this.elm=options.elm;
-	this.time=options.time;
-	this.actions=options.actions;
-	this.callback=options.callback;
+var Animation;
 
-	let {
-		elm,
-		time,
-		actions,
-		callback
-	} = this;
+(function(root, factory) {
 
-	let timer,
-		queue=[],
-		styles= window.getComputedStyle ? window.getComputedStyle(elm) : window.currentStyle(elm);
+	/* CommonJS */
+  	if (typeof exports == 'object') module.exports = factory()
 
-	for(let key in actions){
-		let currentStatus;
+	/* AMD module */
+	else if (typeof define == 'function' && define.amd) define(factory)
 
-		if(key=='scrollTop' || key=='scrollLeft'){
-			currentStatus=elm[key];
-		}else{
-			let style=Number(styles[key]);
-			currentStatus= isNaN(style) ? Number(styles[key].split('px')[0]) : style;
+	/* Global */
+	else root.Animation = factory()
+
+}(this, function() {
+
+	Animation = function(options) {
+
+		var elm = this.elm = options.elm,
+			time = this.time = options.time,
+			actions = this.actions = options.actions,
+			callback = this.callback = options.callback;
+
+		var timer, 
+			queue = [],
+			styles = window.getComputedStyle 
+					? window.getComputedStyle(elm) : window.currentStyle 
+					? window.currentStyle(elm) : elm.currentStyle;
+
+		for(var key in actions) {
+			var currentStatus;
+
+			if(key == 'scrollTop' || key == 'scrollLeft') {
+				currentStatus = elm[key];
+			} else {
+				var style = Number(styles[key]);
+				currentStatus = isNaN(style) ? Number(styles[key].replace('px', '')) : style;
+			};
+
+			queue.push({
+				to: actions[key],
+				from: currentStatus,
+				difference: actions[key] - currentStatus,
+				type: key
+			});
 		};
 
-		queue.push({
-			to:actions[key],
-			from:currentStatus,
-			difference:actions[key]-currentStatus,
-			type:key
-		});
-	};
+		var startTime = new Date().getTime();
 
-	let startTime=new Date().getTime();
+		function go() {
 
-	let go=()=>{
+			timer = setTimeout(function() {
 
-		timer=setTimeout(()=>{
+				var timeDifference = new Date().getTime() - startTime,
+					timeRate = timeDifference / time;
 
-			let timeDifference=new Date().getTime()-startTime,
-				timeRate=timeDifference/time;
+				for(var i = 0; i < queue.length; i ++) {
 
-			for(let i=0;i<queue.length;i++){
+					var type = queue[i].type,
+						unit = isNaN( Number( styles[type] ) ) ? 'px' : '',
+						result = queue[i].difference * timeRate + queue[i].from,
+						isScroll = type == 'scrollTop' || type == 'scrollLeft';
 
-				let type=queue[i].type,
-					unit= isNaN( Number( styles[type] ) ) ? 'px' : '',
-					result=queue[i].from+queue[i].difference*timeRate,
-					isScroll=type=='scrollTop' || type=='scrollLeft';
+					if(timeDifference >= time) {
+						isScroll ? elm[type] = queue[i].to : elm.style[type] = queue[i].to + unit;
+						queue.splice(i, 1);
+						continue;
+					};
 
-				if( timeDifference >= time ){
-					isScroll ? elm[type]=queue[i].to : elm.style[type]=queue[i].to+unit;
-					queue.splice(i,1);
-					continue;
+					isScroll ? elm[type] = result : elm.style[type] = result + unit;
+
 				};
 
-				isScroll ? elm[type]=result : elm.style[type]=result+unit;
+				if(queue.length <= 0) {
+					clearTimeout(timer);
+					callback && callback();
+					return;
+				};
+				
+				go();	
 
-			};
+			}, 0);
+		};
 
-			if(queue.length<=0){
-				clearTimeout(timer);
-				callback && callback();
-				return;
-			};
-
-			go();
-			
-		},0);
+		go();
 
 	};
 
-	go();
+	return Animation;
 
-};
-
-export default Animation;
+}));
